1e1a3be9e7dbac6dbd7916efac85601d69c9b640
Juan José García <jitos86@gmail.com>
2022-12-21 20:45:29 +0100

    change to Ruben's advice

--- js/main.js
+++ js/main.js
@@ -62,7 +62,7 @@
         var typed_strings = $('.typed-text').text();
         var typed = new Typed('.typed-text-output', {
             strings: typed_strings.split(', '),
-            typeSpeed: 200,
+            typeSpeed: 100,
             backSpeed: 20,
             smartBackspace: false,
             loop: true
